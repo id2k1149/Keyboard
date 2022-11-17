71618171fdac99c7b1ad4d19398504abec70fd14
id2k1149 <id2k1149@gmail.com>
2022-11-17 09:01:04 -0500

    edit main storyboard

---
+++ Keyboard/Services/DataSource.swift
@@ -0,0 +1,31 @@
+//
+//  DataSource.swift
+//  Keyboard
+//
+//  Created by Max Franz Immelmann on 11/15/22.
+//
+
+struct DataStore {
+    let layoutUS: [Character]
+    
+}
+
+extension DataStore {
+    
+    static func getLayoutUS() -> [Character] {
+        [
+        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p"
+        ]
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
