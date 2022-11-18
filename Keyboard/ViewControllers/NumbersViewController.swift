9494106b1c577295b4ff48cd7c414f2a8dcdb3f1
id2k1149 <id2k1149@gmail.com>
2022-11-18 16:10:34 -0500

    add numbersCollection

--- Keyboard/ViewControllers/NumbersViewController.swift
+++ Keyboard/ViewControllers/NumbersViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class NumbersViewController: UIViewController {
 
+    @IBOutlet var textField: UITextView!
+    @IBOutlet var keyboard: UIView!
+    
+    @IBOutlet var numbersCollection: [UIButton]!
+    
+    
+    
+    
+    let numbers = Keys().numbersKeys
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // to hide iOS keyboard
+        textField.inputView = UIView()
+        
+        textField.layer.cornerRadius = 10
+        keyboard.layer.cornerRadius = 10
+        
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        
 
 }
