8750a3f57794adb668a602049b2aaaef9daa5d57
id2k1149 <id2k1149@gmail.com>
2022-11-20 14:52:07 -0500

    delete DataSource

--- Keyboard/Models/Keys.swift
+++ Keyboard/Models/Keys.swift
@@ -9,9 +9,22 @@
 class Keys {
     static var shared = Keys()
     
-    let enKeys  = DataStore.shared.enKeys
-    let numbersKeys = DataStore.shared.numbersKeys
-    let symbolsKeys = DataStore.shared.specialKeys
+    let enKeys: [Character]   = [
+        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+        "a", "s", "d", "f", "g", "h", "j", "k", "l",
+        "z", "x", "c", "v", "b", "n", "m"
+    ]
+    let numbersKeys: [Character]  = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+        "-", "/", ":", ";", "(", ")", "$", "&", "@", "\"",
+        ".", ",", "?", "!", "'"
+    ]
+    
+    let symbolsKeys: [Character]  = [
+        "[", "]", "{", "}", "#", "%", "^", "*", "+", "=",
+        "_", "\\", "|", "~", "<", ">", "$", "€", "£", "•",
+        ".", ",", "?", "!", "'"
+    ]
     
     private init() {}
 }
